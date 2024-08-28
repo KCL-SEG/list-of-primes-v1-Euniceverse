28233b729d86fc199245569638e9ec3cbecb3700
Euniceverse <153387754+Euniceverse@users.noreply.github.com>
2024-08-29 01:22:49 +0900

    Update primes.

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    i = 2
+    
+    while(len(list) < number_of_primes): 
+        is_prime = True
+        for y in range(2,int(i ** 0.5) + 1):
+                if i%y == 0:
+                    is_prime = False
+                    break
+        if is_prime:
+            list.append(i)
+        i += 1
     return list
